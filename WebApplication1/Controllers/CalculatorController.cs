cbbf0450f97b70f90b691e1f9f2556b496f34f8f
kityashkina <ekat66215@gmail.com>
2026-02-19 07:31:43 +0300

    Сделана лб1

---
+++ WebApplication1/Controllers/CalculatorController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MathAPI.Models;
+
+namespace MathAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CalculatorController : ControllerBase
+{
+    [HttpPost("calculate")]
+    public double Calculate(CalcRequest request)
+    {
+        // ВНИМАНИЕ: Здесь нужно подставить операцию по твоему варианту!
+        // Вариант 1: Сложение
+        return request.A + request.B;
+
+        // Вариант 2: Умножение
+        // return request.A * request.B;
+
+        // Вариант 3: Деление
+        // return request.A / request.B;
+
+        // Вариант 4: Возведение в степень
+        // return Math.Pow(request.A, request.B);
+    }
+}
---
+++ WebApplication1/Models/CalcRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace MathAPI.Models;
+
+public class CalcRequest
+{
+    public double A { get; set; }
+    public double B { get; set; }
+}
